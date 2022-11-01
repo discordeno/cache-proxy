c2202e529eb88220612dcf72416b7cf9cd41a22c
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2022-11-01 19:57:38 +0100

    removed unwanted side effects

--- setupCacheEdits.ts
+++ setupCacheEdits.ts
@@ -15,11 +15,9 @@ import {
 } from "discordeno/types";
 import { BotWithProxyCache, ProxyCacheTypes } from "./index.js";
 
-export const unavailablesGuilds = new Set<bigint>()
+export const unavailablesGuilds = new Set<bigint>();
 
-export function setupCacheEdits<B extends Bot>(
-  bot: BotWithProxyCache<ProxyCacheTypes, B>,
-) {
+export function setupCacheEdits<B extends Bot>(bot: BotWithProxyCache<ProxyCacheTypes, B>) {
   const {
     GUILD_MEMBER_ADD,
     GUILD_MEMBER_REMOVE,
@@ -38,11 +36,13 @@ export function setupCacheEdits<B extends Bot>(
     const payload = data.d as DiscordGuildMemberAdd;
 
     const guildID = bot.transformers.snowflake(payload.guild_id);
-    const guild = await bot.cache.guilds.get(guildID);
+    const guild = await bot.cache.guilds.get(guildID, false);
     // Update cache
     if (guild) {
       guild.memberCount++;
       await bot.cache.guilds.set(guild);
+    } else {
+      await bot.cache.guilds.get(guildID);
     }
 
     GUILD_MEMBER_ADD(bot, data, shardId);
@@ -52,11 +52,14 @@ export function setupCacheEdits<B extends Bot>(
     const payload = data.d as DiscordGuildMemberRemove;
 
     const guildID = bot.transformers.snowflake(payload.guild_id);
-    const guild = await bot.cache.guilds.get(guildID);
+    const guild = await bot.cache.guilds.get(guildID, false);
     if (guild) {
       guild.memberCount--;
       bot.cache.guilds.set(guild);
+    } else {
+      await bot.cache.guilds.get(guildID);
     }
+
     GUILD_MEMBER_REMOVE(bot, data, shardId);
   };
 
@@ -64,7 +67,7 @@ export function setupCacheEdits<B extends Bot>(
     const payload = data.d as DiscordMessageReactionAdd;
 
     const messageId = bot.transformers.snowflake(payload.message_id);
-    const message = await bot.cache.messages.get(messageId);
+    const message = await bot.cache.messages.get(messageId, undefined, undefined, false);
 
     const emoji = bot.transformers.emoji(bot, payload.emoji);
 
@@ -93,6 +96,8 @@ export function setupCacheEdits<B extends Bot>(
       }
 
       bot.cache.messages.set(message);
+    } else {
+      await bot.cache.messages.get(messageId, BigInt(payload.channel_id), payload.guild_id ? BigInt(payload.guild_id) : undefined);
     }
 
     MESSAGE_REACTION_ADD(bot, data, shardId);
@@ -102,7 +107,7 @@ export function setupCacheEdits<B extends Bot>(
     const payload = data.d as DiscordMessageReactionRemove;
 
     const messageId = bot.transformers.snowflake(payload.message_id);
-    const message = await bot.cache.messages.get(messageId);
+    const message = await bot.cache.messages.get(messageId, undefined, undefined, false);
 
     const emoji = bot.transformers.emoji(bot, payload.emoji);
 
@@ -126,6 +131,8 @@ export function setupCacheEdits<B extends Bot>(
       }
 
       bot.cache.messages.set(message);
+    } else {
+      await bot.cache.messages.get(messageId, BigInt(payload.channel_id), payload.guild_id ? BigInt(payload.guild_id) : undefined);
     }
 
     MESSAGE_REACTION_REMOVE(bot, data, shardId);
@@ -142,6 +149,8 @@ export function setupCacheEdits<B extends Bot>(
       message.reactions = undefined;
 
       bot.cache.messages.set(message);
+    } else {
+      await bot.cache.messages.get(messageId, BigInt(payload.channel_id), payload.guild_id ? BigInt(payload.guild_id) : undefined);
     }
 
     MESSAGE_REACTION_REMOVE_ALL(bot, data, shardId);
@@ -180,7 +189,7 @@ export function setupCacheEdits<B extends Bot>(
       shardId: shardId,
     });
 
-    const oldGuild = await bot.cache.guilds.get(guild.id);
+    const oldGuild = await bot.cache.guilds.get(guild.id, false);
     await bot.cache.guilds.set(guild);
 
     //Send the event.
@@ -196,7 +205,7 @@ export function setupCacheEdits<B extends Bot>(
       guildId: BigInt(payload.guild_id),
     });
 
-    const oldRole = await bot.cache.roles.get(role.id);
+    const oldRole = await bot.cache.roles.get(role.id, undefined, false);
     await bot.cache.roles.set(role);
     //Send the event.
     bot.events.guildRoleUpdateWithOldRole(bot, oldRole!, role);
@@ -205,16 +214,8 @@ export function setupCacheEdits<B extends Bot>(
 
   bot.handlers.GUILD_MEMBER_UPDATE = async function (_, data, shardId) {
     const payload = data.d as DiscordGuildMemberUpdate;
-    const member = bot.transformers.member(
-      bot,
-      payload,
-      BigInt(payload.guild_id),
-      BigInt(payload.user.id),
-    );
-    const oldMember = await bot.cache.members.get(
-      member.id,
-      BigInt(payload.guild_id),
-    );
+    const member = bot.transformers.member(bot, payload, BigInt(payload.guild_id), BigInt(payload.user.id));
+    const oldMember = await bot.cache.members.get(member.id, BigInt(payload.guild_id), false);
     await bot.cache.members.set(member);
     //Send the event.
     bot.events.guildMemberUpdateWithOldMember(bot, oldMember!, member);
@@ -224,9 +225,7 @@ export function setupCacheEdits<B extends Bot>(
   bot.handlers.USER_UPDATE = async function (_, data, shardId) {
     const payload = data.d as DiscordUser;
     const user = bot.transformers.user(bot, payload);
-    const oldUser = await bot.cache.users.get(
-      user.id,
-    );
+    const oldUser = await bot.cache.users.get(user.id, false);
 
     await bot.cache.users.set(user);
 
@@ -242,7 +241,7 @@ export function setupCacheEdits<B extends Bot>(
     // If Guild isn't available push to Set
     if (payload.unavailable) unavailablesGuilds.add(guildID);
     // otherwise remove from Set
-    else unavailablesGuilds.delete(guildID)
+    else unavailablesGuilds.delete(guildID);
 
     GUILD_UPDATE(bot, data, shardId);
   };
